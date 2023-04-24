c9cd4d61e6d8db6c30c856388f710c8055f9c458
RemiErr <Re.Err236@gmail.com>
2023-04-24 17:42:14 +0800

    init

---
+++ Fruit Crush/res/fruit.h
@@ -0,0 +1,17 @@
+#include <string>
+using namespace std;
+
+struct Position
+{
+    int x;
+    int y;
+};
+
+class Fruits
+{
+private:
+    Position pos;
+    string img; // IMG URL
+    bool pick;
+    
+};
---
+++ Fruit Crush/res/functions.cpp
@@ -0,0 +1,6 @@
+#include "board.h"
+#include "fruit.h"
+#include "information.h"
+#include <bits/stdc++.h>
+using namespace std;
+
